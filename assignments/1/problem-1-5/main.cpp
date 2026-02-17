85f5a657ced5615152402a9bc32bc3278393c723
hskramstad00 <hakon.skramstad@nmbu.no>
2026-02-17 20:44:25 +0100

    ferdig?

---
+++ assignments/1/problem-1-5/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+    std::cout << "\nargv[0] er namnet til den k\u00F8yrbare fila: " << argv[0] << "\n";
+
+    std::unordered_set<std::string> seen;
+    std::vector<std::string> unique_args;
+    unique_args.reserve(static_cast<std::size_t>(argc));
+
+    for (int i = 0; i < argc; ++i)
+    {
+        std::string arg(argv[i]);
+
+        // insert returns {iterator, inserted_bool}
+        auto [it, inserted] = seen.insert(arg);
+        if (inserted)
+            unique_args.push_back(std::move(arg));
+    }
+
+    for (const auto& a : unique_args)
+        std::cout << "\targument: \"" << a << "\"\n";
+
+    return 0;
+}
