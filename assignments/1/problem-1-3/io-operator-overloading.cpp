76da377db29f3392b6eb1258466897bdc4b09e24
hskramstad00 <hakon.skramstad@nmbu.no>
2026-02-21 11:56:40 +0100

    3 modified

--- assignments/1/problem-1-3/io-operator-overloading.cpp
+++ assignments/1/problem-1-3/io-operator-overloading.cpp
@@ -1,7 +1,8 @@
 #include "journal-article-index.h"
 #include <iostream>
+#include <sstream>
 #include <fstream>
-#include <string>
+
 
 int main()
 {
@@ -12,42 +13,30 @@ int main()
    jai_i.set_page(152345);
    std::clog << "JournalArticleIndex jai_i:\n" << jai_i << "\n";
 
-   const std::string tmp_filename = "jai_tmp.txt";
+   const std::string filename = "jai_i.txt";
 
-   std::clog << "\nWriting jai_i into a temporary file: " << tmp_filename << "\n";
 
+   std::clog << "\nWriting jai_i into file: <<filename>>.\n";
    {
-      std::ofstream out(tmp_filename, std::ios::out | std::ios::trunc);
-      if (!out) {
-         std::cerr << "ERROR: Could not open file for writing: " << tmp_filename << "\n";
-         return 1;
-      }
-      out << jai_i;
+        std::ofstream out(filename);
+        if (!out) {
+            std::cerr << "kunne ikke åpne fil for skriving: " << filename << "\n";
+            return 1;
+        }
+        out << jai_i;  
    }
 
-   std::clog << "Text content of the file is now as follows:\n";
-   {
-      std::ifstream in(tmp_filename);
-      if (!in) {
-         std::cerr << "ERROR: Could not open file for reading " << tmp_filename << "\n";
-         return 1;
-      }
-      std::string line;
-      while (std::getline(in, line)) {
-         std::clog << line << "\n";
-      }
-   }
+litindex::JournalArticleIndex jai_j;
+std::clog << "\nReading jai_j from file: <<filename>>.\n";
+{
+    std::ifstream in(filename);
+    if (!in) {
+        std::cerr << "kunne ikke åpne fil for lesing: " << filename << "\n";
+        return 1;
+    }
+    in >> jai_j;
+}
+std::clog << "JournalArticleIndex jai_j:\n" << jai_j << "\n";
 
-   litindex::JournalArticleIndex jai_j;
-   std::clog << "\nReading jai_j from the temporary file.\n";
-   {
-      std::ifstream in(tmp_filename);
-      if (!in) {
-         std::cerr << "ERROR: Could not open file for reading " << tmp_filename << "\n";
-         return 1;
-      }
-      in >> jai_j;
-   }
-
-   std::clog << "JournalArticleIndex jai_j:\n" << jai_j << "\n";
+return 0;  
 }
