0f2ac8b22e10d9fad57bb3b9967550f5041b41be
hskramstad00 <hakon.skramstad@nmbu.no>
2026-02-21 14:46:31 +0100

    endra 5 til runar

--- assignments/1/problem-1-5/main.cpp
+++ assignments/1/problem-1-5/main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include <vector>
 
 int main(int argc, char** argv)
 {
-    std::cout << "\nargv[0] er namnet til den k\u00F8yrbare fila: " << argv[0] << "\n";
-
     std::unordered_set<std::string> seen;
-    std::vector<std::string> unique_args;
-    unique_args.reserve(static_cast<std::size_t>(argc));
 
-    for (int i = 0; i < argc; ++i)
-    {
-        std::string arg(argv[i]);
-
-        auto [it, inserted] = seen.insert(arg);
-        if (inserted)
-            unique_args.push_back(std::move(arg));
+    for (int i = 0; i < argc; ++i) {
+        std::string s = argv[i];
+        if (seen.insert(s).second) {         
+            std::cout << "argument: \"" << s << "\"\n";
+        }
     }
-
-    for (const auto& a : unique_args)
-        std::cout << "\targument: \"" << a << "\"\n";
-
-    return 0;
 }
